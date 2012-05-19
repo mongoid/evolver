e48933bdf4acb3d954ed93906a939c787c475151
Durran Jordan <durran@gmail.com>
2012-05-19 10:06:18 +0200

    Flushing out behaviour with specs

---
+++ lib/evolver.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+require "moped"
+require "evolver/version"
+
+module Evolver
+  extend self
+
+  def session
+  end
+
+  def sessions(name)[]
+  end
+end
--- spec/evolver_spec.rb
+++ spec/evolver_spec.rb
@@ -1,1 +1,55 @@
 require "spec_helper"
+
+describe Evolver do
+
+  describe ".session" do
+
+    context "when no configuration is found" do
+
+      it "raises an error" do
+
+      end
+    end
+
+    context "when a config/evolver.yml is found" do
+
+      it "returns the default session" do
+
+      end
+    end
+
+    context "when a config/mongoid.yml is found" do
+
+      it "returns the default session" do
+
+      end
+    end
+  end
+
+  describe ".sessions" do
+
+    context "when providing no name" do
+
+      it "raises an error" do
+
+      end
+    end
+
+    context "when providing a name" do
+
+      context "when the configuration exists" do
+
+        it "returns the named session" do
+
+        end
+      end
+
+      context "when the configuration does not exist" do
+
+        it "raises an error" do
+
+        end
+      end
+    end
+  end
+end
