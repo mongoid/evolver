a039b1c6cd297110ad370d78a6f9b551c587a3df
Durran Jordan <durran@gmail.com>
2012-05-20 16:36:24 +0200

    Add rdoc to time extensions

--- lib/evolver/extensions/time.rb
+++ lib/evolver/extensions/time.rb
@@ -7,12 +7,32 @@ module Evolver
 
       FORMAT = "%Y%m%d%H%M%S"
 
+      # Convert the time to a migration friendly time that will be prefixed in
+      # the file name.
+      #
+      # @example Generate the timestamp.
+      #   time.to_evolver_timestamp
+      #
+      # @return [ String ] The time prefix for the file name.
+      #
+      # @since 0.0.0
       def to_evolver_timestamp
         strftime(FORMAT)
       end
 
       module ClassMethods
 
+        # Instantiate a time object based on the timestamp string that is used
+        # as a file name prefix.
+        #
+        # @example Generate the time from the timestamp string.
+        #   Time.from_evolver_timestamp("20120101120000")
+        #
+        # @param [ String ] timestamp The file prefix timestamp.
+        #
+        # @return [ Time ] The time for the timestamp.
+        #
+        # @since 0.0.0
         def from_evolver_timestamp(timestamp)
           ::Time.strptime(timestamp, FORMAT)
         end
