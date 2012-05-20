e4ef3c5cd26f77cd6f0da556cbdcb8bf8039b5ce
Durran Jordan <durran@gmail.com>
2012-05-20 19:30:24 +0200

    Adding in rake tasks

---
+++ lib/evolver/tasks/rake.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+namespace :evolver do
+
+  desc "Generate a new MongoDB data migration."
+  task generate: :environment do
+    Evolver.generate
+  end
+
+  desc "Execute any pending MongoDB data migrations."
+  task migrate: :environment do
+    Evolver.migrate
+  end
+
+  desc "Revert the last run MongoDB data migration."
+  task revert: :environment do
+    Evolver.revert
+  end
+
+  desc "Get statistics on all pending and run MongoDB data migrations."
+  task stats: :environment do
+    Evolver.stats
+  end
+end
