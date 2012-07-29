7b435e8115b3fe0eb3112206da4b5af8cafd4e2e
Durran Jordan <durran@gmail.com>
2012-07-29 09:45:57 +0200

    Include mongoid for configuration

--- lib/evolver.rb
+++ lib/evolver.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
-require "moped"
+require "mongoid"
 require "evolver/extensions"
 require "evolver/migrator"
 require "evolver/railtie"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,15 @@ require "db/evolver/migrations/20120519113509_rename_bands_to_artists"
 require "db/evolver/migrations/20120520152200_add_likes_to_label"
 
 module Rails
-  def self.root
+  extend self
+
+  def env
+    "test"
+  end
+
+  def root
     File.join(Dir.pwd, "spec")
   end
 end
+
+Mongoid.load!(File.join(File.dirname(__FILE__), "config", "mongoid.yml"))
